ef8f644c42dfa1f2ac9ead4538adea9b31d917b0
Ilya Bashilov <tango1603@yandex.ru>
2017-03-15 00:08:42 +0400

    add FLAGS start activity on receiver.

--- app/src/main/java/com/example/ilya/gb_android2_les5_broadcast/HelloActivity.java
+++ app/src/main/java/com/example/ilya/gb_android2_les5_broadcast/HelloActivity.java
@@ -1,7 +1,9 @@
 package com.example.ilya.gb_android2_les5_broadcast;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class HelloActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        Intent intent = getIntent();
+        Toast.makeText(this, String.valueOf(intent.getIntExtra("level",-1)), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/ilya/gb_android2_les5_broadcast/MyReceive.java
+++ app/src/main/java/com/example/ilya/gb_android2_les5_broadcast/MyReceive.java
@@ -3,6 +3,7 @@ package com.example.ilya.gb_android2_les5_broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.BatteryManager;
 import android.widget.Toast;
 
 /**
@@ -13,11 +14,15 @@ public class MyReceive extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Charge!", Toast.LENGTH_SHORT).show();
-        Intent intent1 = new Intent(context,HelloActivity.class);
+        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        String temp = intent.getStringExtra(BatteryManager.EXTRA_TEMPERATURE);
+
+        Toast.makeText(context, temp, Toast.LENGTH_SHORT).show();
+        Intent intent1 = new Intent(context, HelloActivity.class);
         intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                |Intent.FLAG_ACTIVITY_CLEAR_TASK
-                |Intent.FLAG_ACTIVITY_NEW_TASK);
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent1.putExtra("level", level);
         context.startActivity(intent1);
     }
 }
